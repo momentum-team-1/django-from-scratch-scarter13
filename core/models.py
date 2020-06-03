048b6cb07b29c3c986de88a3fb52e579bfdbd389
scarter13 <scarter13@gmail.com>
2020-06-03 17:37:19 -0400

    relocated models to core from users

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Snippet(models.Model):
+    users = models.ForeignKey(to = User, on_delete = models.CASCADE, related_name = 'snippets')
+    title = models.CharField(max_length=255)
+    date = models.DateField(auto_now_add=True, null=True, blank=True)
+    description = models.CharField(max_length=255)
+    text = models.TextField(null=True, blank=True)
+    #public = 
+
+    def __str(self):
+        return self.text
--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def homepage(request):
+    if request.user.is_authenticated
+        return redirect (to='snippet_list')
+
+    return render(request, "core/home.html")
+
+def snippet_list(request):
+    snippets = request.user.snippets.all()
+    # Clinton had () at the end of this line, but I think he removed them later?
+    return render(request, "core/snippet_list.html", {"snippets": snippets})
