e731c7a4b876af663bb7e77153e4b7d317ce87be
scarter13 <scarter13@gmail.com>
2020-06-03 19:07:13 -0400

    home page appears

--- core/views.py
+++ core/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 
 def homepage(request):
-    if request.user.is_authenticated
-        return redirect (to='snippet_list')
+    #if request.user.is_authenticated:
+        #return redirect (to='snippet_list')
+    return render(request, "pages/home.html")
 
-    return render(request, "core/home.html")
-
-def snippet_list(request):
-    snippets = request.user.snippets.all()
+#def snippet_list(request):
+    #snippets = request.user.snippets.all()
     # Clinton had () at the end of this line, but I think he removed them later?
-    return render(request, "core/snippet_list.html", {"snippets": snippets})
+    #return render(request, "steve_project/snippet_list.html", {"snippets": snippets})
